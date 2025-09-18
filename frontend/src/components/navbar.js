a6bcc8e27d60bb21e1c356bd8ebf98732d6db358
LinardsLiepenieks <linardsliepenieks@gmail.com>
2025-09-18 13:43:34 +0300

    set up tailwind

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,45 +1,46 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-	const location = useLocation();
+  const location = useLocation();
 
-	const isHomePage = location.pathname === "/";
+  const isHomePage = location.pathname === '/';
 
-	const scrollToSection = (id) => {
-		const element = document.getElementById(id);
-		console.log("SCROLL");
-		if (element) {
-			element.scrollIntoView({ behavior: "smooth", block: "start" });
-		}
-	};
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    console.log('SCROLL');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
 
-	return (
-		<nav>
-			<div className="container">
-				<div className="nav-logo">NM</div>
-				<div className="navbar-links">
-					{/* Conditionally render the ul element and its content */}
-					{!isHomePage && (
-						<ul>
-							<li>
-								<Link className="nav-link" to="/">
-									Home
-								</Link>
-							</li>
-							<li>
-								<button
-									className="nav-link"
-									onClick={() => scrollToSection("movies")}>
-									Movies
-								</button>
-							</li>
-						</ul>
-					)}
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav>
+      <div className="container !bg-blue-500">
+        <div className="!bg-blue-500 !text-red-500">NM</div>
+        <div className="navbar-links">
+          {/* Conditionally render the ul element and its content */}
+          {!isHomePage && (
+            <ul>
+              <li>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="nav-link"
+                  onClick={() => scrollToSection('movies')}
+                >
+                  Movies
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
